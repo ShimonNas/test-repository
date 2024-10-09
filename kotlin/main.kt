e24f7894fe26d8484c74960606c9fc0f1a1ca112
ShimonNas <atuyambeshimon@gmail.com>
2024-10-09 16:44:20 +0300

    i have added more information

---
+++ kotlin/main.kt
@@ -0,0 +1,19 @@
+fun main() {  
+    var x =45
+    var y =123
+   var sum = x+y
+   var product = x*y
+   var difference = x-y
+   var z = x/y
+
+
+   println("the sum is: "+sum)
+   println("the product is: "+product)
+   println("the difference is: "+difference)
+   println("the division is: "+z)
+
+
+
+
+
+  }
